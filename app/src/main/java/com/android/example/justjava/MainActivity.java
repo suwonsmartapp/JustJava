38c3ae3f7f9e6e0c1833557bea1906976a12ee91
Junsuk Oh <a811219@gmail.com>
2015-08-25 15:46:55 +0900

    레이아웃 설계 2 주문 버튼 이벤트 처리 추가

--- app/src/main/java/com/android/example/justjava/MainActivity.java
+++ app/src/main/java/com/android/example/justjava/MainActivity.java
@@ -1,11 +1,17 @@
+
 package com.android.example.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final int PRICE_COFFEE = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * 주문 버튼 이벤트 처리
+     * @param view 이벤트 처리 할 view
+     */
+    public void submitOrder(View view) {
+//        Log.d(TAG, "버튼 눌렸다!!!!");
+        display(2);
+        displayPrice(PRICE_COFFEE * 2);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /**
+     * 수량을 화면에 표시
+     * @param number 표시 할 수량
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
 }
